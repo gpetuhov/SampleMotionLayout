68b15bad50443d81e8c3c48dda24c4cf9f9a047c
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-11 18:41:21 +0300

    Set motion listener

--- app/src/main/java/com/gpetuhov/android/samplemotionlayout/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplemotionlayout/MainActivity.kt
@@ -2,7 +2,9 @@ package com.gpetuhov.android.samplemotionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,23 @@ class MainActivity : AppCompatActivity() {
 
         // This is how we trigger our animation
         startAnimation.setOnClickListener { motionContainer.transitionToEnd() }
+
+        motionContainer.setTransitionListener(object: MotionLayout.TransitionListener {
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                seekbar.progress = ceil(p3 * 100).toInt()
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                // Do nothing
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+                // Do nothing
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                // Do nothing
+            }
+        })
     }
 }
